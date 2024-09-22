12010affa06227f760b871981cd3a36031d0fb35
Beatyc <beatyc0288@student.faytechcc.edu>
2024-09-21 23:44:12 -0400

    First submission

---
+++ P1LAB1_BeatyCaden.py
@@ -0,0 +1,17 @@
+####Caden Beaty
+##9/21/2024
+##P1Lab1
+##Using variables to make a sentence
+
+
+print('Enter your first name:', end=' ')
+firstname=input()
+
+print('Enter your last name:', end=' ')
+lastname=input()
+
+print('Hello', firstname, lastname + '! Welcome to CTI-110')
+
+
+
+
