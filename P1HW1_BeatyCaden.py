002bbdc85e3b759e63524f9771d89607cee1d7b0
Beatyc <beatyc0288@student.faytechcc.edu>
2024-09-29 18:31:22 -0400

    Add files via upload

---
+++ P1HW1_BeatyCaden.py
@@ -0,0 +1,27 @@
+##Caden Beaty
+##9/29/2024
+##P1HW1
+##Using integers to perform different mathematical operations.
+
+
+
+
+print('-----Calculating Exponenets----')
+print()
+print('Enter an interger as the base value:', end=' ')
+baseval=int(input())
+
+print('Enter value as the exponenet:', end=' ')
+expval=int(input())
+print()
+print(baseval, 'raised to the power of', expval,  'is:', baseval**expval, '!!')
+print()
+print('------Addition and Subtraction----')
+print()
+print('Enter a starting integer:')
+baseval2=int(input())
+print('Enter an integer to add:')
+addval=int(input())
+print('Enter an integer to subtract:')
+subval=int(input())
+print(baseval2, '+', addval, '-', subval, 'is equal to', baseval2+addval-subval)
